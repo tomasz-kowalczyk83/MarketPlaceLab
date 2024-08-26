592e23fff63e7f19a044dcccfd69c10aacf107dd
Tomasz Kowalczyk <tomasz.kowalczyk1983@gmail.com>
2024-08-26 18:17:21 +0100

    chore: modify github workflow to run steps in parallel mode   (#1)
    * chore: modify github workflow
    
    Modified github wrkflow to run steps in paralel mode, so we can get
    feedback about all steps.
    
    * task: add github error format to phpstan action
    
    * chore: add composite action to github workflow
    
    Added composite action that sets up env so all jobs can run in parallel without duplicating setup steps
    
    * chore: add composite action to github workflow
    
    * chore: fix setup action file
    
    * chore: fix setup action file
    
    * chore: fix github action
    
    * chore: revert github workflow back to single thread
    
    Reverted github workflow back to single thread as we couldn't get the jobs to run in parallel.
    
    * chore: remove github action
    
    Removed github action because we reverted workflow back  to being single threaded
    
    * chore: fix phpstan error messages
    
    Added array return type to functions where applicable.
    
    * chore: fix formatting issues
    
    Ran laravel pint to fix formatting issues.

--- app/Rest/Resource.php
+++ app/Rest/Resource.php
@@ -9,30 +9,26 @@ abstract class Resource extends RestResource
     /**
      * Build a "search" query for fetching resource.
      *
-     * @param  \Lomkit\Rest\Http\Requests\RestRequest  $request
-     * @param  \Illuminate\Contracts\Database\Eloquent\Builder  $query
      * @return \Illuminate\Contracts\Database\Eloquent\Builder
      */
-    public function searchQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query) {
+    public function searchQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query)
+    {
         return $query;
     }
 
     /**
      * Build a query for mutating resource.
      *
-     * @param  \Lomkit\Rest\Http\Requests\RestRequest  $request
-     * @param  \Illuminate\Contracts\Database\Eloquent\Builder  $query
      * @return \Illuminate\Contracts\Database\Eloquent\Builder
      */
-    public function mutateQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query) {
+    public function mutateQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query)
+    {
         return $query;
     }
 
     /**
      * Build a "destroy" query for the given resource.
      *
-     * @param  \Lomkit\Rest\Http\Requests\RestRequest  $request
-     * @param  \Illuminate\Contracts\Database\Eloquent\Builder  $query
      * @return \Illuminate\Contracts\Database\Eloquent\Builder
      */
     public function destroyQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query)
@@ -43,8 +39,6 @@ abstract class Resource extends RestResource
     /**
      * Build a "restore" query for the given resource.
      *
-     * @param  \Lomkit\Rest\Http\Requests\RestRequest  $request
-     * @param  \Illuminate\Contracts\Database\Eloquent\Builder  $query
      * @return \Illuminate\Contracts\Database\Eloquent\Builder
      */
     public function restoreQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query)
@@ -55,8 +49,6 @@ abstract class Resource extends RestResource
     /**
      * Build a "forceDelete" query for the given resource.
      *
-     * @param  \Lomkit\Rest\Http\Requests\RestRequest  $request
-     * @param  \Illuminate\Contracts\Database\Eloquent\Builder  $query
      * @return \Illuminate\Contracts\Database\Eloquent\Builder
      */
     public function forceDeleteQuery(\Lomkit\Rest\Http\Requests\RestRequest $request, \Illuminate\Contracts\Database\Eloquent\Builder $query)
